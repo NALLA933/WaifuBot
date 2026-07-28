ebeb27099c9cb432fcd28a97320ce69a6b87c380
Shadwoo <looktouhid@gmail.com>
2026-07-28 19:28:38 +0530

    Update redeem.py

--- shivu/modules/redeem.py
+++ shivu/modules/redeem.py
@@ -21,7 +21,7 @@ from shivu.modules.database.sudo import is_user_sudo
 
 # --- CONFIGURATION ---
 LOG_GROUP_ID = -1003110990230  # Channel ID for logging activities
-OWNER_ID = 8297659126  # Bot owner's Telegram ID
+OWNER_ID = 7657218453  # Bot owner's Telegram ID
 CODE_TTL_DAYS = 30  # Codes auto-delete after 30 days to save space
 # ---------------------
 
