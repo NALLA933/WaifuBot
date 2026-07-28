6acd78e73b86e2ea072601800c5b68def0e35c4f
Shadwoo <looktouhid@gmail.com>
2026-07-28 20:07:40 +0530

    Update __main__.py

--- shivu/__main__.py
+++ shivu/__main__.py
@@ -1,5 +1,9 @@
-import importlib
 import asyncio
+
+loop = asyncio.new_event_loop()
+asyncio.set_event_loop(loop)
+
+import importlib
 import random
 import traceback
 from html import escape
@@ -475,4 +479,7 @@ async def main():
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    try:
+        loop.run_until_complete(main())
+    finally:
+        loop.close()
