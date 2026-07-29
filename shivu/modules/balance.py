c93719522139d5be364870548004907b9274e71b
Shadwoo <looktouhid@gmail.com>
2026-07-29 22:46:28 +0530

    Update balance.py

--- shivu/modules/balance.py
+++ shivu/modules/balance.py
@@ -1,28 +1,34 @@
 from telegram import Update
-from telegram.ext import CommandHandler, CallbackContext
+from telegram.ext import CommandHandler, ContextTypes
 from shivu import application, user_collection
 
 
 async def get_user(uid):
-    return await user_collection.find_one({'id': uid})
+    return await user_collection.find_one({"id": uid})
 
 
 async def init_user(uid):
-    await user_collection.insert_one({'id': uid, 'balance': 0})
+    user = {"id": uid, "balance": 0}
+    await user_collection.insert_one(user)
+    return user
 
 
-async def balance_cmd(update: Update, context: CallbackContext):
-    if not update.effective_user:
+async def balance_cmd(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if not update.effective_user or not update.message:
         return
 
     uid = update.effective_user.id
-    user = await get_user(uid)
-    if not user:
-        await init_user(uid)
-        user = await get_user(uid)
 
-    balance = int(user.get('balance', 0))
-    await update.message.reply_text(f"💸 ʙᴀʟᴀɴᴄᴇ: `{balance}`")
+    user = await get_user(uid)
+    if user is None:
+        user = await init_user(uid)
+
+    balance = user.get("balance", 0)
+
+    await update.message.reply_text(
+        f"💸 **Balance:** `{balance}`",
+        parse_mode="Markdown",
+    )
 
 
 application.add_handler(CommandHandler("bal", balance_cmd, block=False))
