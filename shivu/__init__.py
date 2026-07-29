04242a653e14629160fd91cb4d08a1eba993ad47
Shadwoo <looktouhid@gmail.com>
2026-07-29 20:17:09 +0530

    Update __init__.py

--- shivu/__init__.py
+++ shivu/__init__.py
@@ -30,8 +30,8 @@ UPDATE_CHAT = Config.UPDATE_CHAT
 BOT_USERNAME = Config.BOT_USERNAME 
 sudo_users = Config.sudo_users
 OWNER_ID = Config.OWNER_ID 
-JOINLOGS = "-1003087506512"
-LEAVELOGS = "-1003087506512"
+JOINLOGS = "-1004298672514"
+LEAVELOGS = "-1004298672514"
 
 application = Application.builder().token(TOKEN).build()
 shivuu = Client("Shivu", api_id, api_hash, bot_token=TOKEN)
