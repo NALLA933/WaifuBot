de23429b8a37a8fde714e416f0f6af307da1d953
Shadwoo <looktouhid@gmail.com>
2026-07-30 17:30:39 +0530

    Update check.py

--- shivu/modules/check.py
+++ shivu/modules/check.py
@@ -113,13 +113,10 @@ def card_caption(char: Char, gcount: int) -> str:
     emoji, text = rarity_parts(char.rarity)
     return (
         "💠 𝗖𝗛𝗔𝗥𝗔𝗖𝗧𝗘𝗥 𝗜𝗡𝗙𝗢:\n"
-        "┏━━━━━━━━━━━━━⧫\n"
         f"◈𝗡𝗔𝗠𝗘: {escape(char.name)}\n"
         f"◈𝗥𝗔𝗥𝗜𝗧𝗬: {emoji} {text}\n"
         f"◈𝗔𝗡𝗜𝗠𝗘: {escape(char.anime)}\n"
-        f"◈𝗣𝗥𝗜𝗖𝗘: {char.price:,}\n\n"
         f"🌍 ɢʟᴏʙᴀʟʟʏ ɢʀᴀʙʙᴇᴅ {gcount}x\n\n"
-        "━━━━━━━━━━━━━━━━━\n"
         "ᴀ ᴘʀᴇᴄɪᴏᴜs ᴄʜᴀʀᴀᴄᴛᴇʀ ᴡᴀɪᴛɪɴɢ ᴛᴏ ᴊᴏɪɴ ʏᴏᴜʀ ᴄᴏʟʟᴇᴄᴛɪᴏɴ"
     )
 
